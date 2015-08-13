b3c7eec443cbc3b5348c08cadc72f94e7dc14b26
Jared Champion <championjared@gmail.com>
2015-08-13 12:35:50 +1000

    Init Residual Analysis File

---
+++ p1_residual_analysis.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*
+# File:: Udacity_P1 - p1_residual_analysis.py
+# Description::
+#   
+# | © Copyright 2015 Llama Logic | #
+__copyright__ = '© 2015 Jared Champion'
+
+__author__ = 'Jared Champion'
+__buildCode__ = 'Udacity_P1.p1_residual_analysis.JC.2015.08.13.12'
+
